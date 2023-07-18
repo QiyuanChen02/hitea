3d5a105ada6804cb915ef4f975ee8b1204535e54
Qiyuan Chen <qiyuan.chen2002@gmail.com>
2023-07-18 17:13:03 +0100

    Added more milkTeaData

--- src/utils/milkTeaData.ts
+++ src/utils/milkTeaData.ts
@@ -14,7 +14,8 @@ export type MilkTeaCategoryType = {
     | "Frappe"
     | "Creamy Cheese Tea"
     | "Mixed Fruit Tea"
-    | "Fizzy Boba";
+    | "Fizzy Boba"
+    | "Coffee";
   teas: TeasType;
 };
 
@@ -92,13 +93,13 @@ export const milkTeaData: MilkTeaDataType = [
         price: 450,
         description: "Japanese Matcha Frappe",
       },
-      //   {
-      //     id: 10,
-      //     name: "Lotus Boboshake Frappe",
-      //     image: "lotusBoboshakeFrappe.png",
-      //     price: 450,
-      //     description: "Lotus Boboshake Frappe",
-      //   },
+      {
+        id: 10,
+        name: "Lotus Boboshake Frappe",
+        image: "lotusBoboshakeFrappe.png",
+        price: 450,
+        description: "Lotus Boboshake Frappe",
+      },
       {
         id: 11,
         name: "Strawberry Yogurt Frappe",
@@ -106,19 +107,151 @@ export const milkTeaData: MilkTeaDataType = [
         price: 450,
         description: "Strawberry Yogurt Frappe",
       },
-      //   {
-      //     id: 12,
-      //     name: "Blueberry Yogurt Frappe",
-      //     image: "blueberryYogurtFrappe.png",
-      //     price: 450,
-      //     description: "Blueberry Yogurt Frappe",
-      //   },
+      {
+        id: 12,
+        name: "Blueberry Yogurt Frappe",
+        image: "blueberryYogurtFrappe.png",
+        price: 450,
+        description: "Blueberry Yogurt Frappe",
+      },
       {
         id: 13,
         name: "Mango Yogurt Frappe",
         image: "mangoYogurtFrappe.jpg",
         price: 450,
         description: "Mango Yogurt Frappe",
+      },
+    ],
+  },
+  {
+    type: "Mixed Fruit Tea",
+    teas: [
+      {
+        id: 14,
+        name: "Mango and Passion Fruit Tea with Popping Boba",
+        image: "oreoBobashakeFrappe.png",
+        price: 450,
+        description: "Mango and Passion Fruit Tea with Popping Boba",
+      },
+      {
+        id: 15,
+        name: "Strawberry and Peach Fruit Tea with Popping Boba",
+        image: "oreoBobashakeFrappe.png",
+        price: 450,
+        description: "Strawberry and Peach Fruit Tea with Popping Boba",
+      },
+      {
+        id: 16,
+        name: "Lychee Ice Green Tea with Popping Boba",
+        image: "oreoBobashakeFrappe.png",
+        price: 450,
+        description: "Lychee Ice Green Tea with Popping Boba",
+      },
+      {
+        id: 17,
+        name: "Jumbo Mixed Fruit Green Tea",
+        image: "oreoBobashakeFrappe.png",
+        price: 500,
+        description: "Jumbo Mixed Fruit Green Tea",
+      },
+      {
+        id: 18,
+        name: "Kiwi Fruit Green Tea with Popping Boba",
+        image: "oreoBobashakeFrappe.png",
+        price: 450,
+        description: "Kiwi Fruit Green Tea with Popping Boba",
+      },
+      {
+        id: 19,
+        name: "Passion Fruit Green Tea with Popping Boba",
+        image: "oreoBobashakeFrappe.png",
+        price: 450,
+        description: "Passion Fruit Green Tea with Popping Boba",
+      },
+    ],
+  },
+  {
+    type: "Creamy Cheese Tea",
+    teas: [
+      {
+        id: 20,
+        name: "Mango and Passion Fruit Cheese Tea Macchiato",
+        image: "oreoBobashakeFrappe.png",
+        price: 450,
+        description: "Mango and Passion Fruit Cheese Tea Macchiato",
+      },
+      {
+        id: 21,
+        name: "Strawberry & Peach Cheese Tea Macchiato",
+        image: "strawberry&PeachCheeseTea.png",
+        price: 450,
+        description: "Strawberry & Peach Cheese Tea Macchiato",
+      },
+      {
+        id: 22,
+        name: "Black Grape Cheese Tea Macchiato",
+        image: "blackGrapeCheeseTea.png",
+        price: 450,
+        description: "Black Grape Cheese Tea Macchiato",
+      },
+      {
+        id: 23,
+        name: "Strawberry Cheese Tea Macchiato",
+        image: "oreoBobashakeFrappe.png",
+        price: 450,
+        description: "Strawberry Cheese Tea Macchiato",
+      },
+      {
+        id: 24,
+        name: "Peach Cheese Tea Macchiato",
+        image: "oreoBobashakeFrappe.png",
+        price: 450,
+        description: "Peach Cheese Tea Macchiato",
+      },
+    ],
+  },
+  {
+    type: "Coffee",
+    teas: [
+      {
+        id: 25,
+        name: "Milk Coffee with Brown Sugar Pearls",
+        image: "oreoBobashakeFrappe.png",
+        price: 350,
+        description: "Milk Coffee with Brown Sugar Pearls",
+      },
+      {
+        id: 26,
+        name: "Coconut Latte with Coffee Jellies",
+        image: "strawberry&PeachCheeseTea.png",
+        price: 450,
+        description: "Coconut Latte with Coffee Jellies",
+      },
+      {
+        id: 27,
+        name: "Black Coffee With Jasmine Tea Cheese Machiato",
+        image: "blackCoffeeWithJasmine.png",
+        price: 450,
+        description: "Black Coffee With Jasmine Tea Cheese Machiato",
+      },
+    ],
+  },
+  {
+    type: "Fizzy Boba",
+    teas: [
+      {
+        id: 28,
+        name: "Blueberry Lemon Fizzy Bubble",
+        image: "oreoBobashakeFrappe.png",
+        price: 350,
+        description: "Blueberry Lemon Fizzy Bubble",
+      },
+      {
+        id: 29,
+        name: "Strawberry Lemon Fizzy Bubble",
+        image: "strawberry&PeachCheeseTea.png",
+        price: 350,
+        description: "Strawberry Lemon Fizzy Bubble",
       },
     ],
   },

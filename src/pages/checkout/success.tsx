6b2a1f83d9367b31101911c884233b2a100cfbe9
Qiyuan Chen <qiyuan.chen2002@gmail.com>
2023-08-11 04:47:47 +0100

    Updated checkout success page

--- src/pages/checkout/success.tsx
+++ src/pages/checkout/success.tsx
@@ -9,8 +9,14 @@ import { type ParsedItemType } from "../items/[id]";
 export default function Success() {
   return (
     <PageWrapper>
-      <div className="mt-3 flex w-full flex-col gap-3 md:w-4/5">
-        <div>Successfully ordered!</div>
+      <div className="mt-3 flex w-full flex-col items-center gap-3 md:w-4/5">
+        <h2 className="text-xl">Thank you for ordering!</h2>
+        <p>
+          To check your current orders, go to the order page{" "}
+          <Link className="text-blue-700" href="/orders">
+            here
+          </Link>
+        </p>
       </div>
     </PageWrapper>
   );
---
+++ src/pages/orders/index.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from "~/components/pagewrapper";
+
+export default function Orders() {
+  return (
+    <PageWrapper>
+      <div className="mt-3 flex w-full flex-col items-center gap-3 md:w-4/5">
+        <h2 className="text-xl">The is the orders page</h2>
+      </div>
+    </PageWrapper>
+  );
+}

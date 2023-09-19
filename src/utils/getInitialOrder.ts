825361341becb2e4496c9716edc30b2bfdf0303d
Qiyuan Chen <qiyuan.chen2002@gmail.com>
2023-09-19 11:40:11 +0100

    minor edit to colourful milk tea

--- src/utils/getInitialOrder.ts
+++ src/utils/getInitialOrder.ts
@@ -8,7 +8,7 @@ export function getInitialOrder(id: number): OrderType {
       Math.floor(id / 100) !== 4 && id !== 204 && id !== 503
         ? "medium"
         : undefined,
-    sweetness: "1",
+    sweetness: Math.floor(id / 100) !== 1 ? "1" : undefined,
     ice: "normal",
     specialInstructions: "",
     hasTea: Math.floor(id / 100) === 2 ? "With Green Tea" : undefined,
